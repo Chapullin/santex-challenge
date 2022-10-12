358c416cdf37039f217a0a41b77d538571cd4ff7
Cristian <molinero.cristian.d@gmail.com>
2022-10-12 13:37:48 -0300

    Update package.json/author and delete unnecessary spaces

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Header } from './components/Header/Header';
 import { ProductList } from './components/ProductList/ProductList';
 import { Provider } from "./hooks/useStateWithStorage";
 
-
 function App() {
   return (
     <Provider>
